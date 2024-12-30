06ee7873acc4ed91ee48d252a58c32cac3d4cd79
Bruce Peters <brucebpeters12@gmail.com>
2024-12-29 18:11:46 -0800

    added branding bible colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // These colors are from the branding bible - don't ask me 🤷‍♂️
+        "red-primary": "#9A0000",
+        "red-secondary": "#C80F14",
+        "grey-primary": "#CCCCCC",
+        "black-primary": "#1A1A1A",
+        "off-black": "#333333",
+      },
+      fontFamily: {
+        brand: ["Teko", "sans-serif"],
+        body: ["Roboto", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
