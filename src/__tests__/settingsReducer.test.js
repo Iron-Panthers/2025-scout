2737d73f0c13a08a4e99cb3fdbfdb89ce2a479f3
Bruce Peters <brucebpeters12@gmail.com>
2024-12-29 20:33:50 -0800

    Added dark mode test

--- src/__tests__/settingsReducer.test.js
+++ src/__tests__/settingsReducer.test.js
@@ -1,3 +1,13 @@
+import { settingsReducer } from "../state/settingsReducer";
+
 describe("settingsReducer", () => {
   // add settingsReducer tests here
+  it("should toggle dark mode", () => {
+    const initialState = {
+      darkMode: false,
+    };
+    const action = { type: "TOGGLE_DARK_MODE" };
+    const newState = settingsReducer(initialState, action);
+    expect(newState.darkMode).toBe(true);
+  });
 });
--- src/__tests__/stateReducer.test.js
+++ src/__tests__/stateReducer.test.js
@@ -1,3 +1,6 @@
 describe("stateReducer", () => {
   // add stateReducer tests here
+  it("should pass this dummy test", () => {
+    expect(true).toBe(true);
+  });
 });
