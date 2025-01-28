dc83cbc7c804f2cc2b6630fea285c80d73140c38
Bruce Peters <brucebpeters12@gmail.com>
2025-01-28 13:23:40 -0800

    fixed overflow issue

--- src/hoc/Layout.jsx
+++ src/hoc/Layout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useSettings } from "../state/state";
 import SubwaySurfers from "../components/SubwaySurfers";
+import { useSettings } from "../state/state";
 
 const Layout = ({ children }) => {
   const [settings, settingsDispatch] = useSettings();
 
   return (
     <div className={settings.darkMode ? "dark" : ""}>
-      <div className="font-body bg-light dark:bg-dark text-black dark:text-white w-screen h-screen transition-colors duration-500">
+      <div className="font-body bg-light dark:bg-dark text-black dark:text-white w-screen h-screen transition-colors duration-500 overflow-scroll">
         {settings.subwaySurf && <SubwaySurfers />}
         {children}
       </div>
