f944c0f266232efbe245767c7383835be3165f3f
Bruce <brucebenjaminpeters@gmail.com>
2025-02-23 13:25:50 -0800

    changed base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: "auto-update" })], // to enable PWA
-  base: "/2025-scout",
+  base: "",
 });
